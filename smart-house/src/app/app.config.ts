ae4312d43616f0b54176246d40f01bb70c9847f1
aglamkhujaev <aglamkhujaev@gmail.com>
2025-11-18 17:24:09 +0500

    fix; eslint config change

--- smart-house/src/app/app.config.ts
+++ smart-house/src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -11,9 +15,9 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     providePrimeNG({
       theme: {
-          preset: Aura
-      }
+        preset: Aura,
+      },
     }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+  ],
 };
